9b28083aa4adbc41c0e6298322dfa26456c4b706
Sandeep Sagar Panjala <SandeepSagar.Panjala@gmail.com>
2026-04-08 10:12:25 -0500

    Initial Commit

---
+++ src/main/java/com/cosmos/api/CosmosJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.cosmos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmosJavaApiApplication.class, args);
+	}
+
+}
